6b6f605406f29fe62526e4b28cfe087213eeb281
julietoge <julietogechi27@gmail.com>
2023-05-24 20:15:29 +0100

    updated of LoanWise-SignUP-Page

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import AboutUs from './Pages/AboutUs/AboutUs'
 import Pricing from './Pages/Pricing/Pricing'
 import Blog from './Pages/Blog/Blog'
 import Register from './components/register'
-import ConfirmEmail from './Pages/Authentication/js/ConfirmEmail';
+import SignUP from './Pages/SignUP/js/SignUP';
 function App() {
   return (
     <div className="App">
