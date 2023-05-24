af6e98897b99dd97603824067ca015ad33410929
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2023-05-24 05:23:07 +0100

    About us page

---
+++ src/Pages/AboutUs/data/profile-data.js
@@ -0,0 +1,34 @@
+
+  const profile = [
+    {
+        image: '/images/mofifo.png',
+        name: 'Mofifoluwa Osakuni',
+        position: 'Team Lead',
+    },
+    {
+        image: '/images/ojasanya.png',
+       name: 'Ojasanya Fatimoh',
+        position: 'Senior Product Designer',
+    },
+    {
+        image: '/images/oluwadamilola.png',
+       name: 'Oluwadamilola Abiodun',
+        position: 'Senior Product Designer',
+    },
+    {
+        image: '/images/nnamdi.png',
+       name: 'Nnamdi Kent',
+        position: 'Product Designer',
+    },
+    {
+        image: '/images/balogun.png',
+       name: 'Balogun Mariam',
+        position: 'Product Designer',
+    },
+    {
+        image: '/images/pelumi.png',
+       name: 'Pelumi Oyediji',
+        position: 'Product Designer',
+    }
+]
+export default profile
