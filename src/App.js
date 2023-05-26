9ede26b6072dbf7f235376876c077a2e3f2eb984
Ogfejiro <114877114+Ogfejiro@users.noreply.github.com>
2023-05-26 12:41:07 -0700

    h2 fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,4 @@
- blog
- HEAD
-
-import './App.css';
-
-function App() {
-return("hello world");
-};
- dev
+ 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home/Home'
 import AboutUs from './Pages/AboutUs/AboutUs'
