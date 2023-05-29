46d51ede75b5ae03d5986e8c659cbe1d369795c7
Ogfejiro <114877114+Ogfejiro@users.noreply.github.com>
2023-05-29 03:18:56 -0700

    Merge branch 'blog' of https://github.com/t1-loanwise/loanwise-b-team into dev

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Home from './Pages/Home/Home'
 import Home from './Pages/Home/Home'
 import AboutUs from './Pages/AboutUs/AboutUs'
 import Pricing from './Pages/Pricing/Pricing'
--- src/Pages/Home/TeamWork/Teamwork.jsx
+++ src/Pages/Home/TeamWork/Teamwork.jsx
@@ -1,6 +1,4 @@
-
 import styles from "../LoanwiseImgText/LwImgTxt.module.css";
-
 import TeamworkImg from "./TeamworkImg";
 import TeamworkText from "./TeamworkText";
 
