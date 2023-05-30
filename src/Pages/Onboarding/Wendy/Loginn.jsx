99ebe3b8b2628687ee5161bc47c0959fd2a5ce51
Ogfejiro <114877114+Ogfejiro@users.noreply.github.com>
2023-05-30 20:02:11 +0000

    testing

--- src/Pages/Onboarding/Wendy/Loginn.jsx
+++ src/Pages/Onboarding/Wendy/Loginn.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import AuthLayout from "../../../components/Layout/AuthLayout";
 import FilledBtn from "../../../components/Button/FilledBtn";
+import "./loginn.css";
+import LoginP from "./LoginP";
 
 const Loginn = () => {
-  return <AuthLayout></AuthLayout>;
+  return (
+    <LoginP
+      heading={"Welcome Back"}
+      subHeading={"Enter your details to sign in"}
+    />
+  );
 };
 
 export default Loginn;
