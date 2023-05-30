102fa867561d34938e74600a6f75aee77842d7cf
Ogfejiro <114877114+Ogfejiro@users.noreply.github.com>
2023-05-30 19:28:49 +0000

    testing authentication

---
+++ src/Pages/Onboarding/Wendy/Loginn.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import AuthLayout from "../../../components/Layout/AuthLayout";
+import FilledBtn from "../../../components/Button/FilledBtn";
+
+const Loginn = () => {
+  return <AuthLayout></AuthLayout>;
+};
+
+export default Loginn;
