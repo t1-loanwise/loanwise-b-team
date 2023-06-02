10f2789ce2563e74a23db489b3c44280fa4d5538
Ogfejiro <114877114+Ogfejiro@users.noreply.github.com>
2023-06-02 09:32:57 -0700

    blog-image

--- src/Pages/Website/Blog/Blogimage.jsx
+++ src/Pages/Website/Blog/Blogimage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import HeroSection from "../../../Images/HeroSection.svg";
+import girlImage from "../../../Images/girlImage.svg";
+import Background from "../../Images/Ellipse 36.svg";
+
 import "./Blog.css";
 
 export const Blogimage = () => {
   return (
     <div className="Blogimages">
-      <img src={HeroSection} alt="" width="90%" />
+      <img src={Background} alt="" />
+      <img src={girlImage} alt="" />
     </div>
   );
 };
