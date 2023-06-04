8c1dc66154fcb08dec982bf3adb90fd54b530be3
julietoge <julietogechi27@gmail.com>
2023-06-04 03:48:53 +0100

    installed SettingsPanel

--- src/Pages/Dashboard/Settings/Setting.jsx
+++ src/Pages/Dashboard/Settings/Setting.jsx
@@ -4,27 +4,41 @@ import Notification from "./Notifications/Notification";
 import SecurityPrivacy from "./Security-Privacy/SecurityPrivacy";
 import DashboardRule from "./Dashboard-Rule/DashboardRule";
 import UserPreference from "./User-Preference/UserPreference";
+import React, { Component } from 'react'
+import {
+    Toolbar, Group, SettingsPanel, Switch, Checkbox, CheckboxGroup,
+    Radio, RadioGroup, Option, Selection, Input, TextArea
+} from 'react-settings-panel'
+
 
 const Setting = () => {
+
+  handleSubmit = (data) => console.log;
+
   return (
-    <div>
-      <div>
+    <SettingsPanel color={'#728ad8'} onSubmit={this.handleSubmit}>
+
+      <Selection >
         <h1>Notifications</h1>
       <Notification />
-      </div>
-      <div>
+      </Selection>
+
+      <Selection >
         <h1>Security & Privacy</h1>
       <SecurityPrivacy />
-      </div>
-      <div>
+      </Selection>
+
+      <Selection >
         <h1>Dashboard Rule</h1>
       <DashboardRule />
-      </div>
-      <div>
+      </Selection>
+
+      <Selection >
         <h1>User Preference</h1>
       <UserPreference />
-      </div>
-    </div>
+      </Selection>
+      
+    </SettingsPanel>
   )
 }
 
