b8a78c3b2d5ce5e6a5ea7d4bdb1bc99e04ca25a2
RosheedatOyindamola <Rosheedat1234@yahoo>
2023-06-05 08:43:12 +0100

    fixed the images error

--- src/Pages/Dashboard/Settings/Setting.jsx
+++ src/Pages/Dashboard/Settings/Setting.jsx
@@ -1,45 +1,9 @@
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-import Notification from "./Notifications/Notification";
-import SecurityPrivacy from "./Security-Privacy/SecurityPrivacy";
-import DashboardRule from "./Dashboard-Rule/DashboardRule";
-import UserPreference from "./User-Preference/UserPreference";
-import React, { Component } from 'react'
-import {
-    Toolbar, Group, SettingsPanel, Switch, Checkbox, CheckboxGroup,
-    Radio, RadioGroup, Option, Selection, Input, TextArea
-} from 'react-settings-panel'
-
+import React from 'react'
 
 const Setting = () => {
-
-  handleSubmit = (data) => console.log;
-
   return (
-    <SettingsPanel color={'#728ad8'} onSubmit={this.handleSubmit}>
-
-      <Selection >
-        <h1>Notifications</h1>
-      <Notification />
-      </Selection>
-
-      <Selection >
-        <h1>Security & Privacy</h1>
-      <SecurityPrivacy />
-      </Selection>
-
-      <Selection >
-        <h1>Dashboard Rule</h1>
-      <DashboardRule />
-      </Selection>
-
-      <Selection >
-        <h1>User Preference</h1>
-      <UserPreference />
-      </Selection>
-      
-    </SettingsPanel>
+    <div>Setting</div>
   )
 }
 
-export default Setting;
+export default Setting
